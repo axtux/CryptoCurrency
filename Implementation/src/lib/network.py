6a0520502e88f33868349ad320327124f2ad4236
Axtux <axtux@hotmail.com>
2017-12-11 02:48:26 +0100

    describe network API

--- Implementation/src/lib/network.py
+++ Implementation/src/lib/network.py
@@ -12,5 +12,27 @@ class Network(object):
             ('127.42.1.3', '8080')
         ]
 
+"""
+All data are returned JSON encoded
+
+Response code :
+    200 => success
+    204 => no content (empty response)
+    4xx => client error
+    5xx => server error
+
+    Master and relay requests:
+GET /blocks/previous_hash
+    get the block after previous_hash
+POST /blocks/
+    submit the new mined block
+
+=== Relay server ===
+GET /transactions/
+    get list of pending transactions
+POST /transactions/
+    submit a new transaction to be processed by the network
+"""
+
 if __name__ == '__main__':
     print(Network.get_relays())
