7f13862b9684ac5e1e29d451b4792819372cbfe4
Axtux <axtux@hotmail.com>
2017-12-12 04:19:20 +0100

    one relay and one updater (persistent)

--- src/miner.py
+++ src/miner.py
@@ -9,20 +9,21 @@ from lib.http_client import RelayClient
 from lib.updater import Updater
 
 class Miner:
+    FLAG=1e6 #number of iteration of mining before check if the block has been found
 
-    FLAG=500000 #number of iteration of mining before check if the block has been found
-
-
-    def __init__(self, blockchain, address, relay):
+    def __init__(self, blockchain, address):
         self.blockchain = blockchain
         self.address = address
-        self.relay = relay
+        self.relay = RelayClient()
+        self.updater = Updater(self.blockchain, self.relay)
         self.flag=0
         self.index=0
 
     def create_block(self):
         self.block= Block(self.blockchain.get_last_hash(),self.address,self.get_ordered_transactions())
-
+        # remove transactions if not valid
+        if not self.block.valid_transactions(self.blockchain):
+            self.block.set_transactions([])
 
     def get_ordered_transactions(self):
         """Allow to choose wich transactions will be placed in the block
@@ -38,34 +39,21 @@ class Miner:
         while(1):
             if(self.flag == 0): #Check if a new block has been found
                 self.flag = Miner.FLAG
-                if self.update_blockchain(): # New block has been found
+                if self.updater.update(): # New block has been found
                     self.create_block()
-            else:
-                self.flag = self.flag - 1
-                self.mine(strategy)
-                if(self.block.is_valid(self.blockchain)):
-                    self.relay.submit_block(self.block)
-                    self.flag = 0 #Need to take new transactions
-
-
-    def update_blockchain(self):
-        updater=Updater(self.blockchain)
-        if updater.update() : #True if there is an update
-            self.blockchain = updater.blockchain
-            return True
-        return False
+            self.flag = self.flag - 1
+            self.mine(strategy)
+            if(self.block.good_difficulty()):
+                self.relay.submit_block(self.block)
+                self.flag = 0 #Need to take new transactions
 
     def mine(self,strategy):
-        digest = self.block.set_proof(strategy(self.index))
+        self.block.set_proof(strategy(self.index))
         self.index = self.index +1
-
-"""
-    def randompow(self,previousPow=0):
-        return random.randint(0,2**256)"""
-
 
 def randompow(previousPow=0):
     return random.randint(0,2**256)
+
 if __name__ == '__main__':
     argc = len(sys.argv)
     if not argc == 2:
@@ -73,5 +61,5 @@ if __name__ == '__main__':
 
     print('Starting miner with address '+str(sys.argv[1]))
     b = Blockchain('miner')
-    miner=Miner(b,sys.argv[1],RelayClient())
+    miner = Miner(b, sys.argv[1])
     miner.run(randompow)
