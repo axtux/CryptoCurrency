2e3f1f4e855bdfa617997b5c200ce0d746117229
Axtux <axtux@hotmail.com>
2017-12-12 03:56:11 +0100

    use new logger, is None

--- src/lib/updater.py
+++ src/lib/updater.py
@@ -1,10 +1,9 @@
-import logging
-
 # local imports
 from lib.blockchain import Blockchain
 from lib.utils import sha_256
 from lib.address import Address
 from lib.http_client import RelayClient
+from lib.log import debug
 
 class Updater(object):
     def __init__(self, blockchain, relay=None):
@@ -13,19 +12,18 @@ class Updater(object):
             self.relay = RelayClient()
         else:
             self.relay = relay
-        self.log = logging.getLogger(__name__)
 
     def update(self):
         """Update the blockchain from relay
            Stop when blockchain is updated (next block is None)
            Return True if updated, False if not
         """
-        self.log.debug('updating blockchain')
+        debug('updating blockchain')
         updated = False
         new_block = self.relay.get_block(self.blockchain.get_last_hash())
-        while new_block != None:
-            self.log.debug('found block '+block.get_hash())
+        while not new_block is None:
+            debug('got block '+new_block.get_hash())
             updated = True
-            self.blockChain.add_block(new_block)
+            self.blockchain.add_block(new_block)
             new_block = self.relay.get_block(self.blockchain.get_last_hash())
         return updated
